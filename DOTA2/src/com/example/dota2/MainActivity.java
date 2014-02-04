024a4e0b58ff27da1083fd38ebc338b77c819fe5
Jesper Anneback <jesper.anneback@gmail.com>
2014-02-04 12:45:09 +0100

    added the new DOTA2 layout

---
+++ DOTA2/src/com/example/dota2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.dota2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//ImageView view = (ImageView)findViewById(R.id.main_image);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-Intnet
-======
-
-Project in course DD2390: Internet programming with Carl Henrik Klåvus
