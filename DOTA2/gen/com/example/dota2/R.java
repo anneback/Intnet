c2b39f88b3a30d7473b42c2082b4efce791bca91
Jesper Anneback <jesper.anneback@gmail.com>
2014-02-14 12:31:02 +0100

    new styles for bars

--- DOTA2/gen/com/example/dota2/R.java
+++ DOTA2/gen/com/example/dota2/R.java
@@ -21,15 +21,47 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int dota2_logo=0x7f020000;
-        public static final int dragonclawhook=0x7f020001;
-        public static final int heroes_hover=0x7f020002;
-        public static final int heroes_selected=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int icon_heroes_tab=0x7f020005;
-        public static final int icon_items_tab=0x7f020006;
-        public static final int items_hover=0x7f020007;
-        public static final int items_selected=0x7f020008;
+        public static final int ab_background_textured_example=0x7f020000;
+        public static final int ab_bottom_solid_example=0x7f020001;
+        public static final int ab_solid_example=0x7f020002;
+        public static final int ab_stacked_solid_example=0x7f020003;
+        public static final int ab_texture_tile_example=0x7f020004;
+        public static final int ab_transparent_example=0x7f020005;
+        public static final int btn_cab_done_default_example=0x7f020006;
+        public static final int btn_cab_done_example=0x7f020007;
+        public static final int btn_cab_done_focused_example=0x7f020008;
+        public static final int btn_cab_done_pressed_example=0x7f020009;
+        public static final int cab_background_bottom_example=0x7f02000a;
+        public static final int cab_background_top_example=0x7f02000b;
+        public static final int dota2_logo=0x7f02000c;
+        public static final int dragonclawhook=0x7f02000d;
+        public static final int heroes_hover=0x7f02000e;
+        public static final int heroes_selected=0x7f02000f;
+        public static final int ic_launcher=0x7f020010;
+        public static final int icon_heroes_tab=0x7f020011;
+        public static final int icon_items_tab=0x7f020012;
+        public static final int items_hover=0x7f020013;
+        public static final int items_selected=0x7f020014;
+        public static final int list_focused_example=0x7f020015;
+        public static final int list_pressed_example=0x7f020016;
+        public static final int menu_dropdown_panel_example=0x7f020017;
+        public static final int progress_bg_example=0x7f020018;
+        public static final int progress_horizontal_example=0x7f020019;
+        public static final int progress_primary_example=0x7f02001a;
+        public static final int progress_secondary_example=0x7f02001b;
+        public static final int selectable_background_example=0x7f02001c;
+        public static final int spinner_ab_default_example=0x7f02001d;
+        public static final int spinner_ab_disabled_example=0x7f02001e;
+        public static final int spinner_ab_focused_example=0x7f02001f;
+        public static final int spinner_ab_pressed_example=0x7f020020;
+        public static final int spinner_background_ab_example=0x7f020021;
+        public static final int tab_indicator_ab_example=0x7f020022;
+        public static final int tab_selected_example=0x7f020023;
+        public static final int tab_selected_focused_example=0x7f020024;
+        public static final int tab_selected_pressed_example=0x7f020025;
+        public static final int tab_unselected_example=0x7f020026;
+        public static final int tab_unselected_focused_example=0x7f020027;
+        public static final int tab_unselected_pressed_example=0x7f020028;
     }
     public static final class id {
         public static final int action_settings=0x7f080009;
@@ -76,6 +108,10 @@ public final class R {
         public static final int title_section3=0x7f05000e;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Example=0x7f060003;
+        public static final int ActionBar_Transparent_Example=0x7f060004;
+        public static final int ActionBarTabStyle_Example=0x7f060007;
+        public static final int ActionButton_CloseMode_Example=0x7f06000a;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -102,5 +138,13 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int DropDownListView_Example=0x7f060006;
+        public static final int DropDownNav_Example=0x7f060008;
+        public static final int PopupMenu_Example=0x7f060005;
+        public static final int ProgressBar_Example=0x7f060009;
+        public static final int Theme_Example=0x7f060002;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Example_Widget=0x7f06000b;
     }
 }
